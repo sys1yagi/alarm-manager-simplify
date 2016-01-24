70556520639d945d08ce34a2bd9f1f9c851be8af
Toshihiro Yagi <toshihiro-yagi@cookpad.com>
2016-01-25 00:47:09 +0900

    First commit.

---
+++ library/src/main/java/com/sys1yagi/alarmmanagersimplify/AlarmProcessor.java
@@ -0,0 +1,9 @@
+package com.sys1yagi.alarmmanagersimplify;
+
+import android.content.Context;
+import android.content.Intent;
+
+public interface AlarmProcessor {
+
+    void process(Context context, Intent intent);
+}
