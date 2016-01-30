0201d42fe8f1e5c3ea4ff421657f220021f0fec9
sys1yagi <sylc.yagi@gmail.com>
2016-01-30 15:02:35 +0900

    Fix reference setting.

---
+++ processor/src/main/java/android/support/v4/content/WakefulBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package android.support.v4.content;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class WakefulBroadcastReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+        // Stub!
+    }
+
+    public static ComponentName startWakefulService(Context context, Intent intent) {
+        // Stub!
+        return null;
+    }
+
+    public static boolean completeWakefulIntent(Intent intent) {
+        // Stub!
+        return false;
+    }
+}
